effaaa21df5151f72b24887f899822d6f29263e2
Yusuke Matsumoto <2matzzz@gmail.com>
2016-10-01 14:22:09 +0900

    separated aws provider

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-# Specify the provider and access details
-provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region = "${var.aws_region}"
-}
-
 resource "aws_vpc" "default" {
     cidr_block = "${var.vpc_cidr_block}"
     enable_dns_hostnames = true
